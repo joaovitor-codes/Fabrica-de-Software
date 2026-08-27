8ba04af22891dcbe9e9578abcfee18dfddc3f819
ewertonlx <ewerton123ofc@gmail.com>
2026-08-26 21:01:16 -0300

    merge branch

--- src/usuario/dtos/usuario.ts
+++ src/usuario/dtos/usuario.ts
@@ -1,6 +1,6 @@
 import { ApiPropertyOptional } from "@nestjs/swagger";
 import { Type } from "class-transformer";
-import { IsDate, IsOptional, IsUrl } from "class-validator";
+import { IsDate, IsEmail, IsNotEmpty, IsOptional, IsUrl } from "class-validator";
 
 export class UpdateUsuarioDto {
     @ApiPropertyOptional({
@@ -20,4 +20,33 @@ export class UpdateUsuarioDto {
     @IsOptional()
     @Type(() => Date)
     dataNascimento?: Date;
+}
+
+export class CreateUsuarioDto {
+    //dados do usuario
+    @ApiPropertyOptional({
+        description: 'URL da foto de perfil do usuário',
+        example: 'https://exemplo.com/fotos/perfil.jpg',
+    })
+    @IsUrl()
+    @IsOptional()
+    fotoPerfilUrl?: string;
+
+    @ApiPropertyOptional({
+        description: 'Data de nascimento do usuário no formato ISO',
+        example: '1995-05-12T00:00:00.000Z',
+        type: String, 
+    })
+    @IsDate()
+    @IsOptional()
+    @Type(() => Date)
+    dataNascimento?: Date;
+
+    //dados da conta
+    @IsNotEmpty()
+    name!: string;
+    @IsEmail()
+    email!: string;
+    @IsNotEmpty()
+    password!: string;
 }
