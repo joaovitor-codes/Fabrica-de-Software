13f9f81671d634dc8022210d5b91ed15115f302d
ewertonlx <ewerton123ofc@gmail.com>
2026-09-11 17:58:13 -0300

    feat: Adicionado novas rotas para: listar, adicionar, remover receitas favoritas

--- src/receita/receita.module.ts
+++ src/receita/receita.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ReceitaService } from './receita.service';
 import { ReceitaController } from './receita.controller';
+import { AuthGuard } from '../auth/auth.guard';
+import { RolesGuard } from '../auth/roles.guard';
 
 @Module({
   controllers: [ReceitaController],
-  providers: [ReceitaService],
+  providers: [ReceitaService, AuthGuard, RolesGuard],
 })
 export class ReceitaModule {}
